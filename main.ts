d7b0aebf1493c450cb8968efe59b8df0ea2790ab
moritz2406 <122538931+moritz2406@users.noreply.github.com>
2023-02-10 12:09:38 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    basic.pause(100)
+    for (let index = 0; index < 4; index++) {
+        pins.analogWritePin(AnalogPin.P0, 1023)
+        pins.analogWritePin(AnalogPin.P0, 0)
+    }
 })
